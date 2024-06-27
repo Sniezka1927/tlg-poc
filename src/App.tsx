99940657e3d2c488f8b6d299b2ce4bcdc3945272
Sniezka <dygons14@gmail.com>
2024-06-27 18:43:59 +0200

    Added sending tx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import {
   AlephiumWalletProvider,
   AlephiumConnectButton,
 } from "@alephium/web3-react";
+import { Send } from "./Send";
 
 function App() {
   return (
     <>
       <h1>PoC - ALPH</h1>
-      <AlephiumWalletProvider network="mainnet">
+      <AlephiumWalletProvider network="testnet">
         <AlephiumConnectButton />
+        <Send />
       </AlephiumWalletProvider>
     </>
   );
---
+++ src/Send.tsx
@@ -0,0 +1,34 @@
+import { DUST_AMOUNT } from "@alephium/web3";
+import { useBalance, useWallet } from "@alephium/web3-react";
+import { useState } from "react";
+
+export const Send = () => {
+  const [txId, setTxId] = useState<string>("");
+  const { signer, account } = useWallet();
+  const { balance } = useBalance();
+
+  const send = async () => {
+    if (!signer) return;
+    if (!signer.nodeProvider) return;
+    const tx = await signer.signAndSubmitTransferTx({
+      signerAddress: account.address,
+      signerKeyType: account.keyType,
+      destinations: [
+        {
+          address: account.address,
+          attoAlphAmount: DUST_AMOUNT,
+        },
+      ],
+    });
+
+    setTxId(tx.txId);
+  };
+
+  return (
+    <div>
+      <button onClick={send}> Send 0.1 ALPH</button> <br />
+      {JSON.stringify(balance)} <br />
+      {txId && <div>Transaction ID: {txId}</div>}
+    </div>
+  );
+};
